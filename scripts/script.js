f2b454b55e3a9e828c8d6cad90b2ab87d0ecc256
Krystal304 <krystalwilliams864@gmail.com>
2024-09-24 14:21:32 -0400

    cache DOM elements

--- scripts/script.js
+++ scripts/script.js
@@ -3,36 +3,43 @@ let heading = document.getElementById('heading')
 document.getElementById('heading').style.color = 'brown';
 heading.style.textAlign ='center';
 
-
-
-let welcomeBtn = document.getElementById ('welcome')
-
-
-
 // Caching DOM Elements
-const cList = document.getElementById("commentList");
-const cInput = document.getElementById("commentInput");
-const cBtn = document.getElementById("addCommentBtn");
-cBtn.addEventListener("click", addComment);
-// Functions
-function addComment() {
-  let comment = cInput.value;
-  //add new comment to cList Elements
-  let listItem = document.createElement("li");
-  listItem.textContent = comment;
-  cList.appendChild(listItem);
-  //clear input Element
-  cInput.value = "";
-  cInput.focus();
-}
 
-welcomeBtn.addEventListener('keypress', handleClick);
-//Functions
-function handleClick(event) {
-  console.log(event.key);
-  if (event.key === 'Enter') {
-    let newHead = document.createElement('h1');
-    newHead.textContent = `Welcome!!`;
-    body[0].appendChild(newHead);
-  }
-}
+let email = document.getElementById('email');
+let password = document.getElementById('psw');
+let confirmPassword = document.getElementById("confirm_psw");
+let registerPassword = document.querySelectorAll("registerbtn");
+
+
+
+// let welcomeBtn = document.getElementById ('welcome')
+
+
+
+// // Caching DOM Elements
+// const cList = document.getElementById("commentList");
+// const cInput = document.getElementById("commentInput");
+// const cBtn = document.getElementById("addCommentBtn");
+// cBtn.addEventListener("click", addComment);
+// // Functions
+// function addComment() {
+//   let comment = cInput.value;
+//   //add new comment to cList Elements
+//   let listItem = document.createElement("li");
+//   listItem.textContent = comment;
+//   cList.appendChild(listItem);
+//   //clear input Element
+//   cInput.value = "";
+//   cInput.focus();
+// }
+
+// welcomeBtn.addEventListener('keypress', handleClick);
+// //Functions
+// function handleClick(event) {
+//   console.log(event.key);
+//   if (event.key === 'Enter') {
+//     let newHead = document.createElement('h1');
+//     newHead.textContent = `Welcome!!`;
+//     body[0].appendChild(newHead);
+//   }
+// }
